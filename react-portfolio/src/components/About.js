21184746e6ce4b895ce0d37dbdfe4b5819c0be4a
Peter Kim <peter.kim@uconn.edu>
2022-12-05 21:39:02 -0500

    about me page

---
+++ react-portfolio/src/components/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function About() {
+	return (
+		<section class="about">
+			<h1 id="about">About Me</h1>
+			<p>
+				Hello! My name is Peter Kim. I've had an interest in computers for as
+				long as I can remember. Soon enough this grew into an interest in
+				programming and web design. I have experience in various languages such
+				as HTML, Java, Scheme, and Python. I am constantly looking to improve
+				and expand upon my skillset.
+			</p>
+		</section>
+	);
+}
+
+export default About;
