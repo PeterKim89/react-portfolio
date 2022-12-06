4dec751146f17bcac8c00707f42d861f352be68a
Peter Kim <peter.kim@uconn.edu>
2022-12-06 00:11:58 -0500

    formstate and handleSubmit

--- react-portfolio/src/components/Contact.js
+++ react-portfolio/src/components/Contact.js
@@ -1,5 +1,21 @@
 import React, { useState } from "react";
 
-function Contact() {}
+function Contact() {
+	const [formState, setFormState] = useState({
+		name: "",
+		email: "",
+		message: "",
+	});
+
+	const [errorMessage, setErrorMessage] = useState("");
+	const { name, email, message } = formState;
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		if (!errorMessage) {
+			console.log("Submit Form", formState);
+		}
+	};
+}
 
 export default Contact;
