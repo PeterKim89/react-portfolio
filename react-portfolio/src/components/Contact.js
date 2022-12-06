f56a6f3df119622da8ca5caf27f1b648d63cffa8
Peter Kim <peter.kim@uconn.edu>
2022-12-06 00:33:59 -0500

    Contact form html

--- react-portfolio/src/components/Contact.js
+++ react-portfolio/src/components/Contact.js
@@ -8,12 +8,12 @@ function Contact() {
 		message: "",
 	});
 
-	const [errorMessage, setErrorMessage] = useState("");
+	const [error, setError] = useState("");
 	const { name, email, message } = formState;
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
-		if (!errorMessage) {
+		if (!error) {
 			console.log("Submit Form", formState);
 		}
 	};
@@ -22,23 +22,64 @@ function Contact() {
 		if (e.target.name === "email") {
 			const isValid = validateEmail(e.target.value);
 			if (!isValid) {
-				setErrorMessage("Your email is invalid.");
+				setError("Your email is invalid.");
 			} else {
-				setErrorMessage("");
+				setError("");
 			}
 		} else {
 			if (!e.target.value.length) {
-				setErrorMessage(`${e.target.name} is required.`);
+				setError(`${e.target.name} is required.`);
 			} else {
-				setErrorMessage("");
+				setError("");
 			}
 		}
 
-		if (!errorMessage) {
+		if (!error) {
 			setFormState({ ...formState, [e.target.name]: e.target.value });
 			console.log("Handle Form", formState);
 		}
 	};
+
+	return (
+		<section>
+			<h1>Contact Me</h1>
+			<form id="contact-form" onSubmit={handleSubmit}>
+				<div>
+					<label htmlLabel="name">Name:</label>
+					<input
+						type="text"
+						name="name"
+						defaultValue={name}
+						onBlur={handleChange}
+					/>
+				</div>
+				<div>
+					<label htmlLabel="email">Email address:</label>
+					<input
+						type="email"
+						name="email"
+						defaultValue={email}
+						onBlur={handleChange}
+					/>
+				</div>
+				<div>
+					<label htmlLabel="message">Message:</label>
+					<textarea
+						name="message"
+						rows="4"
+						defaultValue={message}
+						onBlur={handleChange}
+					/>
+				</div>
+				{error && (
+					<div>
+						<p className="error-message">{error}</p>
+					</div>
+				)}
+				<button type="submit">Submit</button>
+			</form>
+		</section>
+	);
 }
 
 export default Contact;
