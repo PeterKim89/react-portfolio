24f23d96bc7a552e4bbde4c336833834ac5bec5c
Peter Kim <peter.kim@uconn.edu>
2022-12-05 21:58:07 -0500

    footer with github, linkedin, email

---
+++ react-portfolio/src/components/Footer.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Footer() {
+	return (
+		<div className="footer-div">
+			<footer>
+				<ul>
+					<li>
+						<a href="https://www.github.com/peterkim89"></a>
+					</li>
+					<li>
+						<a href="https://www.linkedin.com/in/peter-kim89"></a>
+					</li>
+					<li>
+						<h2>Peter.Kim@uconn.edu</h2>
+					</li>
+				</ul>
+			</footer>
+		</div>
+	);
+}
+
+export default Footer;
