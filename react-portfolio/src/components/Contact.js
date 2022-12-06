f6c56107deb92fd523d6135593d863e1f26f4892
Peter Kim <peter.kim@uconn.edu>
2022-12-06 00:20:27 -0500

    contact form handleChange

--- react-portfolio/src/components/Contact.js
+++ react-portfolio/src/components/Contact.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { validateEmail } from "../../utils/helpers";
 
 function Contact() {
 	const [formState, setFormState] = useState({
@@ -14,6 +15,28 @@ function Contact() {
 		e.preventDefault();
 		if (!errorMessage) {
 			console.log("Submit Form", formState);
+		}
+	};
+
+	const handleChange = (e) => {
+		if (e.target.name === "email") {
+			const isValid = validateEmail(e.target.value);
+			if (!isValid) {
+				setErrorMessage("Your email is invalid.");
+			} else {
+				setErrorMessage("");
+			}
+		} else {
+			if (!e.target.value.length) {
+				setErrorMessage(`${e.target.name} is required.`);
+			} else {
+				setErrorMessage("");
+			}
+		}
+
+		if (!errorMessage) {
+			setFormState({ ...formState, [e.target.name]: e.target.value });
+			console.log("Handle Form", formState);
 		}
 	};
 }
